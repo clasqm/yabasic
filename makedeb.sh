41c2206b5eb4ba8592a61f046ab772407f25468a
Michel Clasquin-Johnson <clasqm@gmail.com>
2018-04-27 19:25:56 +0200

    fixedup I hope

--- makedeb.sh
+++ makedeb.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-cd ..
  dpkg-deb --build yabasic_2.78.5-1
  if test -e yabasic_2.78.5-1.deb
  then
-	mv yabasic_2.78.5-1.deb yabasic_2.78.5-1/build
+	mv yabasic_2.78.5-1.deb ./build
  fi
- cd ./yabasic_2.78.5-1
